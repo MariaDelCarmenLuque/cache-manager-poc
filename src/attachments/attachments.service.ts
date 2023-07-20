e93f762c2985e3fb3beed5c7c622551e075303e6
Maria Luque <marialuque@ravn.co>
2023-07-20 15:19:26 -0500

    refactor: improve logs to test functionality

--- src/attachments/attachments.service.ts
+++ src/attachments/attachments.service.ts
@@ -19,20 +19,26 @@ export class AttachmentsService {
       const att = await this.cacheManagerService.getCacheData(
         `${CachePrefix.INTEGRATION_ATT}_${id}`,
       );
+      console.log('Busco attachment en cache : id ', id, att);
       if (att) {
         cacheAttachments.push(att as AttachmentDto);
+        console.log('Se encontro en cache attachment id : ', id);
       } else {
         cacheMisses.push(id);
+        console.log('No se encontro en cache attachment id : ', id);
       }
     }
-    console.log('Busco los attachments en cache');
 
     if (cacheAttachments.length === integrationIds.length) {
       console.log('Encontre todos los attachment en el cache , los retorno');
+      console.log('----------');
       return cacheAttachments;
     }
 
-    console.log('Hago la consulta para las attachments no encontrados');
+    console.log(
+      'Hago la consulta para las attachments no encontrados ids : ',
+      cacheMisses,
+    );
     const attachmentsByIntegrations = attachments.filter((attachment) =>
       cacheMisses.includes(attachment.integrationId),
     );
@@ -42,11 +48,12 @@ export class AttachmentsService {
         attachmentsByIntegrations.filter(
           (attachment) => attachment.integrationId === id,
         ),
-        15000,
+        100000,
       );
+      console.log('Almaceno en cache attachment id : ', id);
     }
-    console.log('Almaceno los attachments en cache');
-    console.log('Retorne attachments de query');
+    console.log('Retorno attachments de cache y de la consulta');
+    console.log('----------');
     return [...attachmentsByIntegrations, ...cacheAttachments];
   }
 
